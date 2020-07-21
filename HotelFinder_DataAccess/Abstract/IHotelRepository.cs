d06e0c3331281ccaad39faab5be95a833490b636
@ali.alizade <ali.alizada@atm.gov.az>
2020-07-21 12:51:55 +0400

    Hotel Finder App

---
+++ HotelFinder_DataAccess/Abstract/IHotelRepository.cs
@@ -0,0 +1,19 @@
+﻿using HotelFinder.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelFinder_DataAccess.Abstract
+{
+    public interface IHotelRepository
+    {
+        Task<List<Hotel>> GetAllHotels();
+        Task<Hotel> GetHotelById(int id);
+        Task<Hotel> GetHotelByName(string name);
+        Task<Hotel> UpdateHotel(Hotel hotel);
+        Task<Hotel> CreateHotel(Hotel hotel);
+        Task DeleteHotel(int id);
+    }
+}
---
+++ HotelFinder_DataAccess/HotelDbContext.cs
@@ -0,0 +1,21 @@
+﻿using HotelFinder.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelFinder_DataAccess
+{
+    public class HotelDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Server=ALI-5-B6\SQLEXPRESS; Database=HotelDb; Trusted_Connection=True");
+
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+    }
+
+}
